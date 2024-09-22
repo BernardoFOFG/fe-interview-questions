d2683a0035274d7e879e58f104d884df464eee33
BernardoFOFG <bernardofofg@gmail.com>
2024-09-22 19:36:42 -0300

    feat: finish homepage

--- src/layout.tsx
+++ src/layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => {
 	return (
-		<main className="max-w-4xl mx-auto px-2 mt-8 flex flex-col">
+		<main className="max-w-4xl mx-auto px-2 h-screen flex flex-col">
 			{children}
 		</main>
 	);
